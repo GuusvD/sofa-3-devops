5fa99dbc6c92327d29df8990976d9b4c183fc4fb
GuusvD <94607611+GuusvD@users.noreply.github.com>
2024-03-04 11:09:42 +0100

    Add springboot project

---
+++ src/main/java/com/avans/sofa3devops/Sofa3DevopsApplication.java
@@ -0,0 +1,13 @@
+package com.avans.sofa3devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sofa3DevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sofa3DevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avans/sofa3devops/Sofa3DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avans.sofa3devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sofa3DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
