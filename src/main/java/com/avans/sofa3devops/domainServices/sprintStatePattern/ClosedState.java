09a101e4b1b61b36bcb9caa1393515ee13ec172d
GuusvD <94607611+GuusvD@users.noreply.github.com>
2024-03-12 12:32:23 +0100

    Replace logging with custom exception, add sprint state switching controller methods and add tests

--- src/main/java/com/avans/sofa3devops/domainServices/sprintStatePattern/ClosedState.java
+++ src/main/java/com/avans/sofa3devops/domainServices/sprintStatePattern/ClosedState.java
@@ -1,34 +1,27 @@
 package com.avans.sofa3devops.domainServices.sprintStatePattern;
 
+import com.avans.sofa3devops.domainServices.exceptions.InvalidStateException;
 import com.avans.sofa3devops.domainServices.sprintFactoryPattern.ISprint;
-
-import java.util.logging.Logger;
 
 public class ClosedState implements ISprintState {
     private ISprint sprint;
-    final Logger logger = Logger.getLogger(this.getClass().getName());
 
     public ClosedState(ISprint sprint) {
         this.sprint = sprint;
     }
 
     @Override
-    public void createdState() {
-        logger.info("Can't go to 'created' state!");
+    public void inProgressState() throws InvalidStateException {
+        throw new InvalidStateException("Cannot transition to 'in progress' state!");
     }
 
     @Override
-    public void inProgressState() {
-        logger.info("Can't go to 'in progress' state!");
+    public void finishedState() throws InvalidStateException {
+        throw new InvalidStateException("Cannot transition to 'finished' state!");
     }
 
     @Override
-    public void finishedState() {
-        logger.info("Can't go to 'finished' state!");
-    }
-
-    @Override
-    public void closedState() {
-        logger.info("Already in 'closed' state!");
+    public void closedState() throws InvalidStateException {
+        throw new InvalidStateException("Already in 'closed' state!");
     }
 }
--- src/main/java/com/avans/sofa3devops/domainServices/sprintStatePattern/FinishedState.java
+++ src/main/java/com/avans/sofa3devops/domainServices/sprintStatePattern/FinishedState.java
@@ -1,30 +1,23 @@
 package com.avans.sofa3devops.domainServices.sprintStatePattern;
 
+import com.avans.sofa3devops.domainServices.exceptions.InvalidStateException;
 import com.avans.sofa3devops.domainServices.sprintFactoryPattern.ISprint;
-
-import java.util.logging.Logger;
 
 public class FinishedState implements ISprintState {
     private ISprint sprint;
-    final Logger logger = Logger.getLogger(this.getClass().getName());
 
     public FinishedState(ISprint sprint) {
         this.sprint = sprint;
     }
 
     @Override
-    public void createdState() {
-        logger.info("Can't go to 'created' state!");
+    public void inProgressState() throws InvalidStateException {
+        throw new InvalidStateException("Cannot transition to 'in progress' state!");
     }
 
     @Override
-    public void inProgressState() {
-        logger.info("Can't go to 'in progress' state!");
-    }
-
-    @Override
-    public void finishedState() {
-        logger.info("Already in 'finished' state!");
+    public void finishedState() throws InvalidStateException {
+        throw new InvalidStateException("Already in 'finished' state!");
     }
 
     @Override
