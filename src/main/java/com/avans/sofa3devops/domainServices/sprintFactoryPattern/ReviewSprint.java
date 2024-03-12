53e180a0d062a70cec40e26233338729c1596283
GuusvD <94607611+GuusvD@users.noreply.github.com>
2024-03-12 11:17:59 +0100

    Add state pattern for sprint

--- src/main/java/com/avans/sofa3devops/domainServices/sprintFactoryPattern/ReviewSprint.java
+++ src/main/java/com/avans/sofa3devops/domainServices/sprintFactoryPattern/ReviewSprint.java
@@ -1,5 +1,43 @@
 package com.avans.sofa3devops.domainServices.sprintFactoryPattern;
 
+import com.avans.sofa3devops.domain.*;
+import com.avans.sofa3devops.domainServices.sprintStatePattern.ISprintState;
+
+import java.util.Date;
+import java.util.List;
+
 public class ReviewSprint implements ISprint {
+    private ISprintState state;
+    private int number;
+    private Date start;
+    private Date end;
+    private List<BacklogItem> backlog;
+    private List<User> developers;
+    private Document document;
+    private Release release;
+    private Pipeline pipeline;
     private boolean reviewed;
+
+    public ReviewSprint(ISprintState state, int number, Date start, Date end, List<BacklogItem> backlog, List<User> developers, Document document, Release release, Pipeline pipeline, boolean reviewed) {
+        this.state = state;
+        this.number = number;
+        this.start = start;
+        this.end = end;
+        this.backlog = backlog;
+        this.developers = developers;
+        this.document = document;
+        this.release = release;
+        this.pipeline = pipeline;
+        this.reviewed = reviewed;
+    }
+
+    @Override
+    public void setState(ISprintState state) {
+        this.state = state;
+    }
+
+    @Override
+    public ISprintState getState() {
+        return this.state;
+    }
 }
