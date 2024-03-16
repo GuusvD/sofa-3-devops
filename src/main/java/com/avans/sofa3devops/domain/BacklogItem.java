a756253da015717aa8d9b46f2b5ad6c8f572ac9a
GuusvD <94607611+GuusvD@users.noreply.github.com>
2024-03-16 15:04:07 +0100

    Add state pattern for backlog items

--- src/main/java/com/avans/sofa3devops/domain/BacklogItem.java
+++ src/main/java/com/avans/sofa3devops/domain/BacklogItem.java
@@ -1,5 +1,9 @@
 package com.avans.sofa3devops.domain;
 
+import com.avans.sofa3devops.domainServices.backlogStatePattern.IBacklogItemState;
+import com.avans.sofa3devops.domainServices.backlogStatePattern.ToDoState;
+import com.avans.sofa3devops.domainServices.exceptions.InvalidStateException;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -9,4 +13,88 @@ public class BacklogItem {
     private User createdBy;
     private User assignedTo;
     private List<Thread> threads;
+    private IBacklogItemState state;
+
+    public BacklogItem(UUID id, List<Activity> activities, User createdBy, User assignedTo, List<Thread> threads) {
+        this.id = id;
+        this.activities = activities;
+        this.createdBy = createdBy;
+        this.assignedTo = assignedTo;
+        this.threads = threads;
+        this.state = new ToDoState(this);
+    }
+
+    public IBacklogItemState getState() {
+        return state;
+    }
+
+    public void setState(IBacklogItemState state) {
+        this.state = state;
+    }
+
+    public void toDoState() throws InvalidStateException {
+        this.state.toDoState();
+    }
+
+    public void doingState() throws InvalidStateException {
+        this.state.doingState();
+    }
+
+    public void readyForTestingState() throws InvalidStateException {
+        this.state.readyForTestingState();
+    }
+
+    public void testingState() throws InvalidStateException {
+        this.state.testingState();
+    }
+
+    public void testedState() throws InvalidStateException {
+        this.state.testedState();
+    }
+
+    public void doneState() throws InvalidStateException {
+        this.state.doneState();
+    }
+
+    // General methods
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(List<Activity> activities) {
+        this.activities = activities;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public User getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(User assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public List<Thread> getThreads() {
+        return threads;
+    }
+
+    public void setThreads(List<Thread> threads) {
+        this.threads = threads;
+    }
 }
--- src/main/java/com/avans/sofa3devops/domainServices/backlogStatePattern/ToDoState.java
+++ src/main/java/com/avans/sofa3devops/domainServices/backlogStatePattern/ToDoState.java
@@ -1,5 +1,42 @@
 package com.avans.sofa3devops.domainServices.backlogStatePattern;
 
-public class ToDoState {
+import com.avans.sofa3devops.domain.BacklogItem;
+import com.avans.sofa3devops.domainServices.exceptions.InvalidStateException;
 
+public class ToDoState implements IBacklogItemState {
+    private BacklogItem item;
+
+    public ToDoState(BacklogItem item) {
+        this.item = item;
+    }
+
+    @Override
+    public void toDoState() throws InvalidStateException {
+        throw new InvalidStateException("Already in 'to do' state!");
+    }
+
+    @Override
+    public void doingState() {
+        item.setState(new DoingState(item));
+    }
+
+    @Override
+    public void readyForTestingState() throws InvalidStateException {
+        throw new InvalidStateException("Cannot transition to 'ready for testing' state!");
+    }
+
+    @Override
+    public void testingState() throws InvalidStateException {
+        throw new InvalidStateException("Cannot transition to 'testing' state!");
+    }
+
+    @Override
+    public void testedState() throws InvalidStateException {
+        throw new InvalidStateException("Cannot transition to 'tested' state!");
+    }
+
+    @Override
+    public void doneState() throws InvalidStateException {
+        throw new InvalidStateException("Cannot transition to 'done' state!");
+    }
 }
