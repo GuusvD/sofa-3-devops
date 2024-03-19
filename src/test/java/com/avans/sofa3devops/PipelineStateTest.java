074ea7273fdd9d0132dd619095a3195110718220
GuusvD <94607611+GuusvD@users.noreply.github.com>
2024-03-19 17:15:22 +0100

    Add incorrect state switching tests for pipeline state pattern

--- src/test/java/com/avans/sofa3devops/PipelineStateTest.java
+++ src/test/java/com/avans/sofa3devops/PipelineStateTest.java
@@ -1,5 +1,6 @@
 package com.avans.sofa3devops;
 
+import com.avans.sofa3devops.domain.BacklogItem;
 import com.avans.sofa3devops.domain.Pipeline;
 import com.avans.sofa3devops.domainServices.exceptions.InvalidStateException;
 import com.avans.sofa3devops.domainServices.pipelineStatePattern.CancelledState;
@@ -10,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 public class PipelineStateTest {
@@ -73,4 +76,106 @@ public class PipelineStateTest {
 
         assertThat(pipeline.getState()).isInstanceOf(CancelledState.class);
     }
+
+    // Incorrect state switching
+    @Test
+    void givenPipelineWithInitialStateWhenSwitchingStateToFinishedStateThenThrowException() {
+        Pipeline pipeline = new Pipeline("Test");
+
+        InvalidStateException exception = assertThrows(InvalidStateException.class, pipeline::finishedState);
+        assertEquals("Cannot transition to 'finished' state!", exception.getMessage());
+    }
+
+    @Test
+    void givenPipelineWithInitialStateWhenSwitchingStateToFailedStateThenThrowException() {
+        Pipeline pipeline = new Pipeline("Test");
+
+        InvalidStateException exception = assertThrows(InvalidStateException.class, pipeline::failedState);
+        assertEquals("Cannot transition to 'failed' state!", exception.getMessage());
+    }
+
+    @Test
+    void givenPipelineWithExecutedStateWhenSwitchingStateToCancelledStateThenThrowException() throws InvalidStateException {
+        Pipeline pipeline = new Pipeline("Test");
+        pipeline.executedState();
+
+        InvalidStateException exception = assertThrows(InvalidStateException.class, pipeline::cancelledState);
+        assertEquals("Cannot transition to 'cancelled' state!", exception.getMessage());
+    }
+
+    @Test
+    void givenPipelineWithFinishedStateWhenSwitchingStateToExecutedStateThenThrowException() throws InvalidStateException {
+        Pipeline pipeline = new Pipeline("Test");
+        pipeline.executedState();
+        pipeline.finishedState();
+
+        InvalidStateException exception = assertThrows(InvalidStateException.class, pipeline::executedState);
+        assertEquals("Cannot transition to 'executed' state!", exception.getMessage());
+    }
+
+    @Test
+    void givenPipelineWithFinishedStateWhenSwitchingStateToFailedStateThenThrowException() throws InvalidStateException {
+        Pipeline pipeline = new Pipeline("Test");
+        pipeline.executedState();
+        pipeline.finishedState();
+
+        InvalidStateException exception = assertThrows(InvalidStateException.class, pipeline::failedState);
+        assertEquals("Cannot transition to 'failed' state!", exception.getMessage());
+    }
+
+    @Test
+    void givenPipelineWithFinishedStateWhenSwitchingStateToCancelledStateThenThrowException() throws InvalidStateException {
+        Pipeline pipeline = new Pipeline("Test");
+        pipeline.executedState();
+        pipeline.finishedState();
+
+        InvalidStateException exception = assertThrows(InvalidStateException.class, pipeline::cancelledState);
+        assertEquals("Cannot transition to 'cancelled' state!", exception.getMessage());
+    }
+
+    @Test
+    void givenPipelineWithFailedStateWhenSwitchingStateToFinishedStateThenThrowException() throws InvalidStateException {
+        Pipeline pipeline = new Pipeline("Test");
+        pipeline.executedState();
+        pipeline.failedState();
+
+        InvalidStateException exception = assertThrows(InvalidStateException.class, pipeline::finishedState);
+        assertEquals("Cannot transition to 'finished' state!", exception.getMessage());
+    }
+
+    @Test
+    void givenPipelineWithCancelledStateWhenSwitchingStateToExecutedStateThenThrowException() throws InvalidStateException {
+        Pipeline pipeline = new Pipeline("Test");
+        pipeline.executedState();
+        pipeline.failedState();
+        pipeline.cancelledState();
+
+        InvalidStateException exception = assertThrows(InvalidStateException.class, pipeline::executedState);
+        assertEquals("Cannot transition to 'executed' state!", exception.getMessage());
+    }
+
+    @Test
+    void givenPipelineWithCancelledStateWhenSwitchingStateToFinishedStateThenThrowException() throws InvalidStateException {
+        Pipeline pipeline = new Pipeline("Test");
+        pipeline.executedState();
+        pipeline.failedState();
+        pipeline.cancelledState();
+
+        InvalidStateException exception = assertThrows(InvalidStateException.class, pipeline::finishedState);
+        assertEquals("Cannot transition to 'finished' state!", exception.getMessage());
+    }
+
+    @Test
+    void givenPipelineWithCancelledStateWhenSwitchingStateToFailedStateThenThrowException() throws InvalidStateException {
+        Pipeline pipeline = new Pipeline("Test");
+        pipeline.executedState();
+        pipeline.failedState();
+        pipeline.cancelledState();
+
+        InvalidStateException exception = assertThrows(InvalidStateException.class, pipeline::failedState);
+        assertEquals("Cannot transition to 'failed' state!", exception.getMessage());
+    }
+
+    //
+
 }
